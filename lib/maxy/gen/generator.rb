a4a5462cbab7226cc877048d6f4c8857d15a877f
Julian Rubisch <julian@julianrubisch.at>
2018-06-01 13:28:31 +0200

    Make the generator return something

--- lib/maxy/gen/generator.rb
+++ lib/maxy/gen/generator.rb
@@ -4,9 +4,29 @@ require 'psych'
 module Maxy
   module Gen
     class Generator
+      TEMPLATE = Psych.load_file(File.join(__dir__, '../../../assets/blank.yml'))
+
+      def initialize
+        @object_count = 0
+        @patch = TEMPLATE
+      end
+
       def generate(node)
-        return JSON.generate(Psych.load_file(File.join(__dir__, '../../../assets/blank.yml'))) if node.nil?
+        return JSON.generate(@patch) if node.nil?
+
+        @patch['patcher']['boxes'] = []
+        @patch['patcher']['boxes'] << 'test'
+        JSON.generate(@patch)
       end
     end
+
+    Box = Struct.new( :id,
+                      :maxclass,
+                      :numinlets,
+                      :numoutlets,
+                      :outlettype,
+                      :patching_rect,
+                      :style,
+                      :text)
   end
 end
--- spec/maxy/generator_spec.rb
+++ spec/maxy/generator_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe Generator do
     expect(Generator.new.generate(nil)).to eq(JSON.generate(Psych.load_file(File.join(__dir__, '../../assets/blank.yml'))))
   end
 
+  it 'should generate two boxes and a line' do
+    tree = ObjectNode.new('sig~', '100.', [ObjectNode.new('*~', '0.5', [])])
+
+    expect(JSON.parse(Generator.new.generate(tree))['patcher']['boxes']).not_to be_empty
+  end
+
 end
