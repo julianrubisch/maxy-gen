f4db10f213aca31f80421474e8a5980d369e7d75
Julian Rubisch <julian@julianrubisch.at>
2018-06-04 09:02:09 +0200

    Use generated library and raise error if not present

--- lib/maxy/gen/generator.rb
+++ lib/maxy/gen/generator.rb
@@ -5,16 +5,20 @@ module Maxy
   module Gen
     class Generator
       TEMPLATE = Psych.load_file(File.join(__dir__, '../../../assets/blank.yml')).freeze
-      LIBRARY = Psych.load_file(File.join(__dir__, '../../../assets/library.yml')).freeze
       OFFSET_X = 20
       OFFSET_Y = 20
       STEP_Y = 40
       HEIGHT = 22
       WIDTH = 50
 
+      attr_accessor :library
+
       def initialize
+        raise RuntimeError.new('No object definitions were found. please run `maxy-gen install` first') unless File.exist?("#{ENV['HOME']}/.maxy-gen/library.yml")
+
         @object_count = 1
         @patch = TEMPLATE.freeze
+        @library = Psych.load_file("#{ENV['HOME']}/.maxy-gen/library.yml").freeze
       end
 
       def generate(node)
@@ -36,7 +40,7 @@ module Maxy
       end
 
       def make_box(node, id)
-        box = LIBRARY['objects'][node.name]
+        box = @library[:objects][node.name]
         box['id'] = id
         box['patching_rect'] = [OFFSET_X, OFFSET_Y + @object_count * STEP_Y, box['width'] || WIDTH, box['height'] || HEIGHT]
         unless box['text'].nil?
--- spec/maxy/generator_spec.rb
+++ spec/maxy/generator_spec.rb
@@ -6,6 +6,11 @@ include Maxy::Gen
 
 RSpec.describe Generator do
 
+  it 'should raise an error if library is not installed' do
+    allow(File).to receive(:exist?) { false }
+    expect { Generator.new.generate(nil) }.to raise_error(RuntimeError)
+  end
+
   it 'should generate the empty template if the argument is nil' do
     expect(Generator.new.generate(nil)).to eq(JSON.generate(Psych.load_file(File.join(__dir__, '../../assets/blank.yml'))))
   end
