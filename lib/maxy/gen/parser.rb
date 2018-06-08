fb1ac9ad7a5266f1e22e89c339f418e464361720
Julian Rubisch <julian@julianrubisch.at>
2018-06-08 20:40:11 +0200

    Correctly parse a nested group

--- lib/maxy/gen/parser.rb
+++ lib/maxy/gen/parser.rb
@@ -6,36 +6,50 @@ module Maxy
 
         @library = Psych.load_file("#{ENV['HOME']}/.maxy-gen/library.yml").freeze
         @tokens = tokens
+        @tree = RootNode.new([])
+        @groups = []
       end
 
-      def parse
-        parse_group_or_obj
+      def parse(parent_node=@tree, closing_group=false)
+        unless closing_group
+          parse_begin_group parent_node
+          child_node = parse_identifier parent_node
+          parse_dash child_node
+        end
+
+        parse_plus parent_node
+
+        parse_end_group
       end
 
-      def parse_group_or_obj(obj_node=nil)
-        parse_group
-
-        parse_plus(obj_node)
-
-        node = parse_obj(obj_node)
-
-        parse_plus(obj_node)
-
-        parse_dash(node)
-
-        node
+      def parse_begin_group(parent)
+        if peek(:oparen)
+          consume(:oparen)
+          @groups << parent
+        end
       end
 
-      def parse_obj(parent_node=nil)
-        return if @tokens.length == 0
+      def parse_end_group
+        return @tree if @tokens.empty?
+        if peek(:cparen)
+          consume(:cparen)
+          parse(@groups.pop, true)
+        end
+      end
 
-        obj_name = parse_identifier
-        puts obj_name
+      def parse_identifier(parent)
+        if peek(:identifier)
+          obj_name = consume(:identifier).value
+        elsif peek(:escaped_identifier)
+          obj_name = consume(:escaped_identifier).value
+        end
 
         arguments = parse_arguments || ''
 
+        raise RuntimeError.new("Could not find #{obj_name} in object definitions.") if @library[:objects][obj_name].nil?
+
         new_obj_node = ObjectNode.new(obj_name, arguments, [])
-        parent_node.child_nodes << new_obj_node unless parent_node.nil?
+        parent.child_nodes << new_obj_node
 
         new_obj_node
       end
@@ -45,19 +59,6 @@ module Maxy
           args = consume(:arguments)
           args.value =~ /\A{([^{}]*)}\Z/
           $1
-        end
-      end
-
-      def parse_group
-        if peek(:oparen)
-          consume(:oparen)
-          puts 'after oparen'
-
-          parse_group_or_obj
-
-          consume(:cparen)
-          puts 'after cparen'
-          puts @tokens.inspect
         end
       end
 
@@ -74,42 +75,23 @@ module Maxy
         @tokens.length > 0 && @tokens.fetch(0).type == expected_type
       end
 
-      def parse_identifier
-        if peek(:identifier)
-          obj_name = consume(:identifier).value
-        end
-
-        if peek(:escaped_identifier)
-          obj_name = consume(:escaped_identifier).value
-        end
-
-        raise RuntimeError.new("Could not find #{obj_name} in object definitions.") if @library[:objects][obj_name].nil?
-
-        obj_name
-      end
-
       def parse_plus(obj_node)
         if peek(:plus)
           consume(:plus)
-          sibling_obj_name = parse_identifier
-          sibling_args = parse_arguments || ''
-          sibling_obj_node = ObjectNode.new(sibling_obj_name, sibling_args, [])
-          obj_node.child_nodes << sibling_obj_node
-
-          parse_plus(obj_node)
-
-          parse_dash(sibling_obj_node)
+          parse(obj_node)
         end
       end
 
       def parse_dash(obj_node)
         if peek(:dash)
           consume(:dash)
-          parse_group_or_obj(obj_node)
+          parse(obj_node)
         end
       end
     end
 
     ObjectNode = Struct.new(:name, :args, :child_nodes, :x_rank, :y_rank)
+    RootNode = Struct.new(:child_nodes)
+
   end
 end
--- spec/maxy/parser_spec.rb
+++ spec/maxy/parser_spec.rb
@@ -11,9 +11,9 @@ RSpec.describe Parser do
               Token.new(:identifier, 'dac~')]
 
     tree = Parser.new(tokens).parse
-    expect(tree.name).to eq('cycle~')
-    expect(tree.child_nodes[0].name).to eq('\*~')
-    expect(tree.child_nodes[0].child_nodes[0].name).to eq('dac~')
+    expect(tree.child_nodes[0].name).to eq('cycle~')
+    expect(tree.child_nodes[0].child_nodes[0].name).to eq('\*~')
+    expect(tree.child_nodes[0].child_nodes[0].child_nodes[0].name).to eq('dac~')
   end
 
   it 'should parse an object with arguments' do
@@ -24,10 +24,10 @@ RSpec.describe Parser do
               Token.new(:arguments, '{named_print}')]
 
     tree = Parser.new(tokens).parse
-    expect(tree.name).to eq('route')
-    expect(tree.args).to eq('fps vol')
-    expect(tree.child_nodes[0].name).to eq('print')
-    expect(tree.child_nodes[0].args).to eq('named_print')
+    expect(tree.child_nodes[0].name).to eq('route')
+    expect(tree.child_nodes[0].args).to eq('fps vol')
+    expect(tree.child_nodes[0].child_nodes[0].name).to eq('print')
+    expect(tree.child_nodes[0].child_nodes[0].args).to eq('named_print')
   end
 
   it 'should raise a parsing error when an unknown object is passed' do
@@ -53,12 +53,12 @@ RSpec.describe Parser do
               Token.new(:identifier, 'cycle~')]
 
     tree = Parser.new(tokens).parse
-    expect(tree.name).to eq('loadbang')
-    expect(tree.child_nodes.size).to eq(3)
-    expect(tree.child_nodes[2].child_nodes.size).to eq(1)
+    expect(tree.child_nodes[0].name).to eq('loadbang')
+    expect(tree.child_nodes[0].child_nodes.size).to eq(3)
+    expect(tree.child_nodes[0].child_nodes[2].child_nodes.size).to eq(1)
   end
 
-  it 'should parse a simple group' do
+  it 'should parse a nested group' do
     tokens = [Token.new(:identifier, 'inlet'),
               Token.new(:dash, '-'),
               Token.new(:oparen, '('),
@@ -69,15 +69,20 @@ RSpec.describe Parser do
               Token.new(:plus, '+'),
               Token.new(:oparen, '('),
               Token.new(:identifier, 'trigger'),
+              Token.new(:arguments, '{b}'),
               Token.new(:dash, '-'),
+              Token.new(:oparen, '('),
               Token.new(:identifier, 'outlet'),
+              Token.new(:plus, '+'),
+              Token.new(:identifier, 'print'),
+              Token.new(:cparen, ')'),
               Token.new(:cparen, ')')]
     tree = Parser.new(tokens).parse
 
-    puts tree.inspect
-    expect(tree.name).to eq('inlet')
-    expect(tree.child_nodes.size).to eq(3)
-    expect(tree.child_nodes[1].child_nodes.size).to eq(2)
-    expect(tree.child_nodes[2].child_nodes.size).to eq(2)
+    expect(tree.child_nodes[0].name).to eq('inlet')
+    expect(tree.child_nodes[0].child_nodes.size).to eq(2)
+    expect(tree.child_nodes[0].child_nodes[0].child_nodes.size).to eq(1)
+    expect(tree.child_nodes[0].child_nodes[1].child_nodes.size).to eq(2)
   end
+
 end
